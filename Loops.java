5fe2d76fab9624db122529107a2d2ded009bbcd1
apcs <apcs@mvhs-u528ap08.dcsdk12.org>
2013-09-03 09:07:20 -0600

    Changes

--- Loops.java
+++ Loops.java
@@ -1,16 +1,31 @@
 public class Loops{
 	public static void main(String[] args) {
+		//String s = "My String";
+		//int length = findLength(s);
+		//System.out.println(length);
 		
+		//int x= 45;
+		//while (x >= 20) {
+			//System.out.println(x);
+			//x= x-5;
 
+		//}
+		//for (int i=24;i<=68 ;i+=2) {
+			//System.out.println(i);
+			
+		//}
+		System.out.println("There are " + drugPotency(.04, 50) + " month(s) left. DONT DIE!");
+		
+	}
+	public static int drugPotency(double decrease, double limit){
+		int month = 0;
 
-		int x =1;
-		while (x <= 100) {
-			System.out.println(x);
-			x++;
-
+		for (double i = 100.0; i > limit; i= i - (i * decrease)) {
+			System.out.println(i);	
+			month++;
+			
 		}
-		System.out.println("DONE BITCH");
-		
+		return month;
 	}
 	
 }
--- README.md
+++ README.md
@@ -6,3 +6,10 @@ iteration
 While and for loops
 
 #Latest Edit
+#HW
+Attepmt to get java and git hug woking at home,
+bring questions on thursday if it fails.
+
+Chapter 9 - Problems 3 and 5 - synce to GitHub.
+Install java jdk.
+Set path variable for java. Or eclipse.
